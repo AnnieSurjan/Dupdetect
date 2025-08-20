71e72ea09993714d50885170ab37207b96b1a3b4
AnnieSurjan <surjaneniko@gmail.com>
2025-08-20 23:54:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
-from flask import Flask, redirect, request
+import os
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'DupDetect működik!'
-
-@app.route('/callback')
-def callback():
-    code = request.args.get('code')
-    return f'Kaptunk kódot: {code}'
-
-if __name__ == '__main__':
-
-    app.run(port=8000)
-    import os
-
+# Környezeti változók beolvasása
 client_id = os.getenv("QB_CLIENT_ID")
 client_secret = os.getenv("QB_CLIENT_SECRET")
 access_token = os.getenv("QB_ACCESS_TOKEN")
 
+# Ellenőrzés: kiírjuk, hogy sikerült-e beolvasni
 print("Client ID:", client_id)
 print("Client Secret:", client_secret)
 print("Access Token:", access_token)
